6ecd509ae2e29fa7ac7b2e06cbcbcc32c61591e4
Mansi Kumawat <mansikumawat42137@gmail.com>
2025-05-01 12:35:18 +0530

    01 May 2025

--- Angular-simple/src/app/login/login.component.ts
+++ Angular-simple/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -13,20 +14,27 @@ export class LoginComponent {
     message: '',
   }
 
-  constructor(private router:Router) {
+  constructor(private httpClient:HttpClient , public router:Router) {
 
   }
 
   signIn() {
-    console.log('in sign in function')
-    console.log('form.data:', this.form.data)
-    
+    this.httpClient.post('http://localhost:8000/ORSAPI/login/',this.form.data).subscribe((res:any)=>{
+      console.log('res=>',res)
+      this.form.message='';
 
-    if (this.form.data.loginId == 'admin' && this.form.data.password == 'admin') {
-      this.router.navigateByUrl('welcome')
-    } else {
-      this.form.message = 'Login ID & Password Invalid'
-    }
+      if(res.result.message){
+        this.form.message=res.result.message;
+      }
+
+      if(res.result.data !=null){
+        localStorage.setItem('firstName',res.result.data.firstName)
+        this.router.navigateByUrl('welcome');
+      }
+      console.log('data',res.result.data)
+      console.log('LocalStorage:', localStorage.getItem('firstName'));
+
+    })
   }
 
   signUp() {
--- Angular-simple/src/app/navbar/navbar.component.ts
+++ Angular-simple/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+  form: any = {
+    data: {}
+  }
+
+  constructor(private router: Router) { }
+
+  isLogin() {
+    let check = localStorage.getItem('firstName');
+    
+    if (check != "null" && check != null) {
+      this.form.data.firstName = localStorage.getItem("firstName");
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  logout() {
+    localStorage.clear();
+    this.router.navigateByUrl('/login')
+  }
+
 
 }
